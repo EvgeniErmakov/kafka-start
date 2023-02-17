0518acb1819ceb322f9980030a10d3f72cafea51
eermakov <EVErmakov@lanit.ru>
2023-02-17 21:17:13 +0300

    add kafka consumer

---
+++ src/main/java/com/example/kafkastart/KafkaListeners.java
@@ -0,0 +1,14 @@
+package com.example.kafkastart;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners  {
+
+    @KafkaListener(topics = "third_topic", groupId = "groupId")
+    void listener(String data) {
+        System.out.println("Listener received : " + data + "!!!");
+    }
+
+}
