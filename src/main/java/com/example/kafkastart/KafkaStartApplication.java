456a067f18d9b8c3165f14f86086103a6879fa35
eermakov <EVErmakov@lanit.ru>
2023-02-19 12:41:25 +0300

    feat: add a way to config kafka by application.yml file

--- src/main/java/com/example/kafkastart/KafkaStartApplication.java
+++ src/main/java/com/example/kafkastart/KafkaStartApplication.java
@@ -1,10 +1,7 @@
 package com.example.kafkastart;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaStartApplication {
@@ -12,20 +9,4 @@ public class KafkaStartApplication {
     public static void main(String[] args) {
         SpringApplication.run(KafkaStartApplication.class, args);
     }
-
-    @Bean
-    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
-        return args -> {
-            kafkaTemplate.send("third_topic", "hello kafka11!");
-        };
-    }
-
-    /*
-    @Bean
-    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
-        return args -> {
-            template.send("topic1", "test");
-        };
-    }
-     */
 }
